7ad9ee5e51ab54f3f9a344e6493bac23d8aa10df
mktbsh <t93.hasebe@gmail.com>
2023-07-07 22:58:39 +0900

    set path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/open-shut-zip',
-  plugins: [svelte()],
+  plugins: [svelte(), tsconfigPaths()],
   build: {
     emptyOutDir: true,
     outDir: 'docs'
